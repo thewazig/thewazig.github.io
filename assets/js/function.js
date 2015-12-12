62bfd18082895345a255670128b5033916e6a7a6
thewazig <thewazig@terra.com.br>
2015-12-12 16:00:35 -0200

    cell

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,5 +1,56 @@
-$(document).ready(function(){$(function(){$('a[href^="#"]').on('click',function(e){e.preventDefault();var target=$(this.hash);$('html, body').stop().animate({'scrollTop':target.offset().top},500,'swing',function(){window.location.hash=none;});});})
-$(function(){$(window).on('scroll',function(){var wScroll=$(this).scrollTop();if(wScroll>$('#aboutMe').offset().top-1){$('.up').fadeIn('slow');}else{$('.up').fadeOut('slow');}})})
-$(function(){var didScroll,navHeight=$('nav').outerHeight(),scrollTop=$(window).scrollTop();$(window).on('scroll',function(){didScroll=true;});setInterval(function(){if(didScroll){hadScroll();didScroll=false;}},250);function hadScroll(){var currentScroll=$(window).scrollTop();if(currentScroll>scrollTop&&currentScroll>navHeight){$('nav').addClass('hidden');}else{$('nav').removeClass('hidden');}scrollTop=currentScroll;}})
-$(function(){$(window).on('scroll',function(){var ScrollTop=$(this).scrollTop(),wHeight=$(window).height()/1.25;if(ScrollTop>$('#servicesWrapper').offset().top-wHeight){$('.articles').css({'animation':'servicesDown 1.2s ease-in-out forwards'});$('.design .servImg, .developement .servImg').css({'animation':'imageServices 0.8s ease-in-out 1.2s forwards'});$('.social .servImg, .more .servImg').css({'animation':'imageServices 0.8s ease-in-out 1.4s forwards'});}if(ScrollTop>$('.briefing').offset().top-wHeight-50){$('.briefing').css({'animation':'howLeft 1.1s ease-in-out forwards'});$('.briefing .elipse').css({'animation':'elipse 0.8s ease-in-out 0.8s forwards'});}if(ScrollTop>$('.planning').offset().top-wHeight-50){$('.planning').css({'animation':'howRight 1.1s ease-in-out forwards'});$('.planning .elipse').css({'animation':'elipse 0.8s ease-in-out 0.8s forwards'});}if(ScrollTop>$('.formalizing').offset().top-wHeight-50){$('.formalizing').css({'animation':'howLeft 1.1s ease-in-out forwards'});$('.formalizing .elipse').css({'animation':'elipse 0.8s ease-in-out 0.8s forwards'});}if(ScrollTop>$('.creating').offset().top-wHeight-50){$('.creating').css({'animation':'howRight 1.1s ease-in-out forwards'});$('.creating .elipse').css({'animation':'elipse 0.8s ease-in-out 0.8s forwards'});}if(ScrollTop>$('.developing').offset().top-wHeight-50){$('.developing').css({'animation':'howLeft 1.1s ease-in-out forwards'});$('.developing .elipse').css({'animation':'elipse 0.8s ease-in-out 0.8s forwards'});}if(ScrollTop>$('.finishing').offset().top-wHeight-50){$('.finishing').css({'animation':'howRight 1.1s ease-in-out forwards'});$('.finishing .elipse').css({'animation':'elipse 0.8s ease-in-out 0.8s forwards'});}})})
-$(function(){var date=new Date().getFullYear();$('footer p').html('© '+date+' Washington Ferreira all rights reserved');})})
+$(document).ready(function(){
+    $(function(){$('a[href^="#"]').on('click',function(e){e.preventDefault();var target=$(this.hash);$('html, body').stop().animate({'scrollTop':target.offset().top},500,'swing',function(){window.location.hash=none;});});})
+
+    $(function(){$(window).on('scroll',function(){var wScroll=$(this).scrollTop();if(wScroll>$('#aboutMe').offset().top-1){$('.up').fadeIn('slow');}else{$('.up').fadeOut('slow');}})})
+
+    $(function(){
+        var didScroll,
+        navHeight=$('.navTrigger').outerHeight(),
+        scrollTop=$(window).scrollTop();
+        
+        $(window).on('scroll',function(){
+            didScroll=true;});
+        
+        setInterval(function(){
+            if(didScroll){
+                hadScroll();
+                didScroll=false;
+            }},250);
+        
+        function hadScroll(){
+            var currentScroll=$(window).scrollTop();
+            if(currentScroll>scrollTop){
+                $('.navTrigger').css({
+                    'top' : '-10em',
+                    'transition' : '0.3s'
+                });
+            }else{
+                $('.navTrigger').css({
+                    'top' : '2em',
+                    'transition' : '0.3s'
+                });
+            }
+            scrollTop=currentScroll;
+        }})
+    
+    $(function (){
+        $('.navTrigger ').on('click', function (){
+            $('nav').css({
+                'left' : '0',
+                'transition' : '0.5s'
+            });
+        });
+        $('nav').on('click', function (){
+            $('nav').css({
+                'left' : '-100vw',
+                'transition' : '0.8s'
+            });
+        });
+    })
+
+    $(function () {
+        var date = new Date().getFullYear();
+        $('footer p').html('© '+date+' Washington Ferreira. All rights reserved');
+    })
+
+})
